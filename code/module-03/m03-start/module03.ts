7d71d65ad00c896638c4ce85859676c9c113a0a7
Daniel <virusxd521@gmail.com>
2022-07-03 09:27:07 +0300

    Finished interfaces exercise

--- code/module-03/m03-start/module03.ts
+++ code/module-03/m03-start/module03.ts
@@ -3,34 +3,39 @@
 
 /*  EXERCISE 1
     TODO: Declare the Loan interface. */
-
+interface Loan {
+    principle : number;
+    interestRate : number;
+}
 
 /*  TODO: Declare the ConventionalLoan interface. */
-
+interface ConventionalLoan extends Loan {
+    months : number;
+}
 
 
 /*  TODO: Update the calculateInterestOnlyLoanPayment function. */
 
-function calculateInterestOnlyLoanPayment(principle, interestRate) {
+function calculateInterestOnlyLoanPayment(loanTerms : Loan) {
     // Calculates the monthly payment of an interest only loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest;
+    payment = loanTerms.principle * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 
-function calculateConventionalLoanPayment(principle, interestRate, months) {
+function calculateConventionalLoanPayment(loanTerms : ConventionalLoan) {
     // Calculates the monthly payment of a conventional loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+    payment = loanTerms.principle * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment(30000, 5);
-let conventionalPayment = calculateConventionalLoanPayment(30000, 5, 180);
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate : 5});
+let conventionalPayment = calculateConventionalLoanPayment({principle: 30000, interestRate : 5, months: 180});
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
